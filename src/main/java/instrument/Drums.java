4b39eace9678f549b2849b05edcea8b79736c620
Christopher Pearson <cpearson3140@gmail.com>
2020-06-07 14:58:26 +0100

    updated Drums & DrumsTest files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/instrument/Drums.java
+++ src/main/java/instrument/Drums.java
@@ -5,13 +5,27 @@ import instruments.IPlay;
 public class Drums extends Instrument {
 
     private int pedals;
+    private int purchasePrice;
+    private int sellingPrice;
 
-    public Drums(String name) {
+    public Drums(String name, int pedals, int purchasePrice, int sellingPrice) {
         super(name);
         this.pedals = pedals;
+        this.purchasePrice = purchasePrice;
+        this.sellingPrice = sellingPrice;
     }
 
     public int getPedals() {
         return pedals;
+    }
+
+    @Override
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    @Override
+    public int getSellingPrice() {
+        return sellingPrice;
     }
 }
--- src/test/java/instrumentTest/DrumsTest.java
+++ src/test/java/instrumentTest/DrumsTest.java
@@ -1,4 +1,37 @@
 package instrumentTest;
 
+import instrument.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void before(){
+        drums = new Drums("Roland", 2, 60000, 75000);
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Roland", drums.getName());
+    }
+
+    @Test
+    public void getPedals(){
+        assertEquals(2, drums.getPedals());
+    }
+
+    @Test
+    public void getPurchasePrice(){
+        assertEquals(60000, drums.getPurchasePrice());
+    }
+
+    @Test
+    public void getSellingPrice(){
+        assertEquals(75000, drums.getSellingPrice());
+    }
 }
