913a2964afa83618d986b2618404506555fcec85
Christopher Pearson <cpearson3140@gmail.com>
2020-06-07 15:15:35 +0100

    updated Keyboard & KeyboardTest files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/test/java/instrumentTest/KeyboardTest.java
+++ src/test/java/instrumentTest/KeyboardTest.java
@@ -4,6 +4,8 @@ import instrument.Keyboard;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class KeyboardTest {
     
     Keyboard keyboard;
@@ -14,6 +16,22 @@ public class KeyboardTest {
     }
 
     @Test
-    public void getName() {
+    public void getName(){
+        assertEquals("Yamaha", keyboard.getName());
+    }
+
+    @Test
+    public void getMusicSheet(){
+        assertEquals(4, keyboard.getMusicSheet());
+    }
+
+    @Test
+    public void getPurchasePrice(){
+        assertEquals(1500, keyboard.getPurchasePrice());
+    }
+
+    @Test
+    public void getSellingPrice() {
+       assertEquals(1800, keyboard.getSellingPrice());
     }
 }
